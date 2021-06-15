f280d98e06916169c59495dc72cdb5929d007124
nicosoto0 <nsoto@miningtag.cl>
2021-06-14 23:07:04 -0400

    implementation ready

--- src/modules/arguments_handler.py
+++ src/modules/arguments_handler.py
@@ -17,32 +17,37 @@ def get_arguments(config_init: ConfigParser,
     Args:
         config_init [ConfigParser]: Initailization configuration file.
         env [str]: Name of environment from where to load initialization variables.
-        
+
     Return:
         [Namespace] with arguments.
     """
-    
-        
+
+
     parser = ArgumentParser()
-    
+
     ## -- [function] ARGUMENT --
     parser.add_argument('--function', type=int, required=False,
                         default=config_init[env]['FUNCTION'],
                         help="Number of function too execute.")
 
-    
+
     if env == 'DEFAULT':
-        ## -- [function] ARGUMENT --
+        ## -- [max_distance] ARGUMENT --
         parser.add_argument('--max_distance', type=int, required=False,
                             default=config_init[env].getfloat('MAX_DISTANCE'),
                             help="Distance to activate switch.")
-    
-    
-        ## -- [function] ARGUMENT --
+
+
+        ## -- [threshold_distance] ARGUMENT --
         parser.add_argument('--threshold_distance', type=int, required=False,
                             default=config_init[env].getfloat('THRESHOLD_DISTANCE'),
                             help="Distance to activate switch.")
-    
+
+        ## -- [max_time_on] ARGUMENT --
+        parser.add_argument('--max_time_on', type=int, required=False,
+                            default=config_init[env].getfloat('MAX_TIME_ON'),
+                            help="Max time switch ON")
+
+
+
     return parser.parse_args()
-    
-    
